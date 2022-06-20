8bea8f70f2df61ecc2b9ae25e58fe8e097ca3212
Moghanavathanan Jeyavathanan <mjeyavat@1-D-6.42heilbronn.de>
2022-06-20 13:45:20 +0200

    gotta finish

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 15:09:35 by mjeyavat          #+#    #+#             */
-/*   Updated: 2022/06/19 15:22:03 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2022/06/20 13:20:31 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int main()
 {
-    std::string s1 = "hallo";
-    std::string s2 = "tis";
+    std::string s1 = "Now";
+    std::string s2 = "later";
     std::string path = "./test.txt";
     
     reWrite r = reWrite(path, s1, s2);
--- cpp_01/ex04/rewrite.cpp
+++ cpp_01/ex04/rewrite.cpp
@@ -10,44 +10,43 @@ reWrite::reWrite(std::string &filePath, std::string &s1, std::string &s2) : _fil
 void reWrite::copyContent()
 {
     std::string line;
+    int len = this->_s1.length();
     std::ifstream InFile(this->_filePath);
     std::fstream OutFile("copy.txt");
-
+    int i = 0;
     if (InFile && OutFile)
     {
         while (std::getline(InFile, line))
         {
-            OutFile << line << '\n';
+
+            std::string::size_type pos = 0;
+            std::cout<<"line[" << i << "]: " << line << '\n';
+            // while (true)
+            // {
+                pos = line.find(this->_s1);
+                if (pos != std::string::npos)//falls es nicht groesser als npos ist somit ist das wort gefundee
+                {
+                    std::string found = line.substr(pos, len);
+                    line.erase(pos, found.length());
+                    std::cout << found << '\n';
+                    line.insert(pos,this->_s2);
+                    std::cout << "the replaced line: " << line << '\n';
+                    OutFile << line << '\n';
+                    pos = 0;
+                    i = 1;
+                }
+                else
+                    break;
+            // }
+            if (i != 1)
+                OutFile << line << '\n';
+            i++;
         } 
         std::cout << "Copy finished" << '\n';
     }
     else
         std::cout << "unable to open file"<< '\n';
     InFile.close();
-    
-    if (OutFile.is_open())
-    {
-        while (getline(OutFile, line))
-        {
-            std::string::size_type pos = line.find(this->_s1);
-            if (pos != std::string::npos)
-            {
-                line.
-            }
-        }
-    }
-    
     OutFile.close();
     
-
-    // if (file.good())
-    // {
-    //     while (std::getline(file,line))
-    //     {
-    //         std::cout << line << '\n';
-    //     }
-    //     file << this->_s2;
-    // }
-    // else
-    // file.close();
 }
