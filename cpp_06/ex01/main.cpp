b9e3c3cf3a60f7297ed955a4bad015e69d554292
Moghanavathanan Jeyavathanan <mjeyavat@1-C-1.42heilbronn.de>
2022-07-15 17:36:14 +0200

    cpp06 ex01 maybe done

--- cpp_06/ex01/main.cpp
+++ cpp_06/ex01/main.cpp
@@ -6,13 +6,29 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 15:50:44 by mjeyavat          #+#    #+#             */
-/*   Updated: 2022/07/14 17:46:25 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2022/07/15 17:35:27 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Data.hpp"
+#include "inc/Data.hpp"
 
 int main()
 {
+	Data *d = new Data;
+
+	d->_name = "Ami";
+
+	std::cout << "struct d: " << d->_name << std::endl;
+
+	uintptr_t numPtr = serialize(d);
+
+	std::cout << "uint numPtr: " << numPtr << std::endl;
 	
+	Data *d2;
+
+	d2 = deserialize(numPtr);
+	
+	std::cout << "struct d2: " << d2->_name << std::endl;
+
+	return (0);
 }
